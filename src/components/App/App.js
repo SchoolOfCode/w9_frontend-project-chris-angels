7ef26f1b3dd5081684f64f42555bb88164cd17a2
brycen <brycenbb1996@gmail.com>
2022-06-20 16:43:40 +0100

    user auth works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import LoginButton from '../Login/index.js';
 import LogoutButton from '../Logout/index.js';
+import Profile from '../Profile/index.js';
 function App() {
   return (
     <div className="App">
-      <LoginButton /> <LogoutButton />
+      <LoginButton /> <LogoutButton /> <Profile></Profile>
     </div>
   );
 }
