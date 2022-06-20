6b691d64de3c7f264056631119fdafd5b104cb22
brycen <brycensoc@gmail.com>
2022-06-20 15:54:07 +0100

    initial commit - boilerplate incomplete

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginButton from '../Login/index.js';
+import LogoutButton from '../Logout/index.js';
+function App() {
+  return (
+    <div className="App">
+      <LoginButton /> <LogoutButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App/App.js';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Auth0Provider domain="" clientId="" redirectUri={window.location.origin}>
+      {' '}
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
