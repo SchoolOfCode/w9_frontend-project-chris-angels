3fa94fd94011025bfd564388a054f6ce8e43c2fe
brycen <brycensoc@gmail.com>
2022-06-21 12:30:32 +0100

    conditionally showing log in/log out

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,10 +8,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 function App() {
   const [user, setUser] = useState({});
+  console.log(user);
+  let islogged = false;
+  if (Object.keys(user).length !== 0) {
+    islogged = true;
+  }
   return (
     <Router>
       <div className="App">
-        <LoginButton /> <LogoutButton /> <Profile addUser={setUser}></Profile>
+        {!islogged && <LoginButton />}
+        {islogged && <LogoutButton />}
+        <Profile addUser={setUser}></Profile>
       </div>
       <Header />
       <Routes>
---
+++ src/components/Settings/index.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import LogoutButton from '../Logout';
+function Settings({ user }) {
+  return (
+    <main>
+      <form></form>
+      <LogoutButton></LogoutButton>
+    </main>
+  );
+}
+export default Settings;
