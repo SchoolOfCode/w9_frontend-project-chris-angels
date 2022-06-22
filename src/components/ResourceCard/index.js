ba8192157e56cee236b1bc3f1670efdf19dba24d
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-06-22 14:23:42 +0100

    Revert "Revert "Fetch resources""

--- src/components/ResourceCard/index.js
+++ src/components/ResourceCard/index.js
@@ -5,13 +5,14 @@ function ResourceCard(props) {
     <div >
       <div className="resourceCard">
         <img className="resourceImage" src={props.logo} alt="resource type logo"></img>{' '}
-        <div className="title">{props.title}Title</div>
+        <div className="title">{props.link}Title</div>
+
       </div>
-      <ol className="tags">
+      <dl className="tags">
         {props.tags.map((tag, index) => {
           return <li key={index}>{tag}<img alt="tag" className="imageTag" src={Tag}></img></li>;
         })}
-      </ol>
+      </dl>
     </div>
   );
 }
