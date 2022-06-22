16aeee9b0d06f57fc0a9fb22c9e718b18108229d
brycen <brycensoc@gmail.com>
2022-06-22 12:34:12 +0100

    set up for diary

---
+++ src/components/Diary/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header';
+function Diary(props) {
+  return (
+    <main>
+      <Header></Header>
+      <section>
+        <div>hi</div>
+      </section>
+    </main>
+  );
+}
+
+export default Diary;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <nav className="navbar">
-      <Link className="homeLink" to="/">Home</Link>
-      <Link className="resourcesLink" to="/resources">Resources</Link>
+      <Link className="homeLink" to="/">
+        Home
+      </Link>
+      <Link className="resourcesLink" to="/resources">
+        Resources
+      </Link>
+      <Link className="diaryLink" to="/diary">
+        Diary
+      </Link>
     </nav>
   );
 }
