90038ad39bd5266d96c201aa6645f75a30f547d5
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-06-23 14:05:20 +0100

    asset added, hintlist completed

--- src/components/Hints/hintList.js
+++ src/components/Hints/hintList.js
@@ -1,14 +1,14 @@
 const hintList = [
 
-    { hint: "hint one" },
-    { hint: "hint two" },
-    { hint: "hint three" },
+    { hint: "Find out where the error is happening, in your console it will tell you which file and line of code" },
+    { hint: "Write down the name of the error you are getting" },
+    { hint: "Start at a place you know is working, console log each subsequent step" },
     { hint: "hint four" },
     { hint: "hint five" },
     { hint: "hint six" },
     { hint: "hint seven" },
     { hint: "hint eight" },
-    
+
 ]
 
 export default hintList
