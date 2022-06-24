ed5c75a334604e922ea03c2225aaa7e67b61de51
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-06-24 09:21:36 +0100

    started work without branching

---
+++ src/components/Panic3/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Panic3() {
+    return (
+        <main>
+            <div>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </main>
+    )
+}
+
+export default Panic3
