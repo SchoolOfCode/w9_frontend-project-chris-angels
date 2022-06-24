cafe2c584d6c7f8b81d0214668cccf394b65fb40
brycen <brycensoc@gmail.com>
2022-06-24 10:50:35 +0100

    shows helpers

--- src/components/Panic3/index.js
+++ src/components/Panic3/index.js
@@ -1,40 +1,46 @@
 // get the problem variables from the previous pa
 
-
-import React, { useState, useEffect } from 'react'
-import pic from '../../Assets/dont-panic-stars.png'
-
+import React, { useState, useEffect } from 'react';
+import pic from '../../Assets/dont-panic-stars.png';
 
 function Panic3({ state }) {
+  const [helpers, setHelpers] = useState([{ slackusername: '' }]);
 
-    const [helpers, setHelpers] = useState([{ slackusername: "" }])
-
-useEffect(() => {
+  useEffect(() => {
     async function fetchHelpers() {
-            let response = await fetch(
-                `http://localhost:3001/help?topic=${state.topic}`
-            );
-            let json = await response.json();
-            let dataArr = json.data;
-            console.log(dataArr)
-            setHelpers([dataArr]);
-        }
-        fetchHelpers()
-    }, []);
-    console.log( helpers.slackusername )
-    return (
-        <main style={{ color: "white", backgroundImage: `url(${pic})`, backgroundSize: "cover" }} className="main-panic3">
-            <div className='problem-request'>
-                <p>{`Hello I've been working on ${state.topic}`}</p>
-                <p>{`The problem is ${state.problem}`}</p>
-                <p>{`I've tried ${state.tried}`}</p>
-            </div>
+      let response = await fetch(
+        `http://localhost:3001/help?topic=${state.topic}`
+      );
+      let json = await response.json();
+      let dataArr = json.data;
+      console.log(dataArr);
+      setHelpers([dataArr]);
+    }
+    fetchHelpers();
+  }, []);
+  console.log('asdasd', helpers);
+  return (
+    <main
+      style={{
+        color: 'white',
+        backgroundImage: `url(${pic})`,
+        backgroundSize: 'cover',
+      }}
+      className="main-panic3"
+    >
+      <div className="problem-request">
+        <p>{`Hello I've been working on ${state.topic}`}</p>
+        <p>{`The problem is ${state.problem}`}</p>
+        <p>{`I've tried ${state.tried}`}</p>
+      </div>
 
-            <div className='problem-helpers'>
-                <p>{helpers.slackusername}</p>
-            </div>
-        </main>
-    )
+      <section className="problem-helpers">
+        {helpers[0].map((helper, index) => {
+          return <p key={index}>{helper.slackusername}</p>;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default Panic3
+export default Panic3;
