51efa910a5efd08d527cb52c22b313b1d797eefb
brycen <brycensoc@gmail.com>
2022-06-24 09:27:10 +0100

    adjusted App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,27 +9,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 
 /*
-  This component serves all of the routes of the application. It keeps track of if a user is logged in or not through the use of the user state. 
-  The user state is used to dicate whether the log in or log out buttons are used inside of the Settings and Home components. It is also used as a base of information
+  This component serves all of the routes of the application. It keeps track of if a user is logged 
+  in or not through the use of the user state. 
+  The user state is used to dicate whether the log in or log out buttons are used inside of 
+  the Settings and Home components. It is also used as a base of information
   for the Diary component.
 
-  Note: Certain routes are protected from being accessed unless a guest is logged in. This is handled through Auth0 directly and not through the user state.
+  Note: Certain routes are protected from being accessed unless a guest is logged in.
+  This is handled through Auth0 directly and not through the user state.
 
 */
 function App() {
   const [user, setUser] = useState({});
-  // console.log('hi', user);
-  // if (
-  //   Object.keys(user).length === 0 &&
-  //   document.getElementById('profile-container') !== null
-  // ) {
-  //   setUser({
-  //     email: document.getElementById('profile-email').textContent,
-  //     name: document.getElementById('profile-name'),
-  //     picture: document.getElementById('profile-picture'),
-  //     username: document.getElementById('profile-slackusername'),
-  //   });
-  // }
   return (
     <Router>
       <Routes>
