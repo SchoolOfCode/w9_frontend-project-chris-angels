10bb5368b6ce8e75e8624d392b3ba630e7840475
brycen <brycensoc@gmail.com>
2022-06-29 10:39:14 +0100

    linear,logout,login refactored

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 import Button from '@mui/material/Button';
-/*Login button component from auth0, used alongside the logout button component and the profile component*/
+
+/* Login button component from auth0, used alongside the logout button 
+   component and the profile component
+*/
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
