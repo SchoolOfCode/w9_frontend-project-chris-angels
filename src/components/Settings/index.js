5a273fda57291347427eaad396ec88ed3a773fb1
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-06-29 11:17:34 +0100

    moved settings css styles into settings folder

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -5,6 +5,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import Box from '@mui/material/Box';
 import { useAuth0 } from '@auth0/auth0-react';
 import Header from '../Header';
+import './Settings.css'
 
 /**
  *
