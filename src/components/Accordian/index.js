ae44c64df2b8208a5ed6d1308736c979308cf978
brycen <brycensoc@gmail.com>
2022-06-30 12:34:37 +0100

    removed typograhpy tag to remove errors

--- src/components/Accordian/index.js
+++ src/components/Accordian/index.js
@@ -1,10 +1,10 @@
-import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Tag from "../../Assets/Tag.png";
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Tag from '../../Assets/Tag.png';
 
 export default function SimpleAccordion(props) {
   let filteredArr = [];
@@ -22,9 +22,9 @@ export default function SimpleAccordion(props) {
         /*The sx prop in the Accordion (and other) element allows styling of the MUI elements directly in JSX */
 
         return (
-          <Accordion key={index} sx={{ border: "1px solid #181e50" }}>
+          <Accordion key={index} sx={{ border: '1px solid #181e50' }}>
             <AccordionSummary
-              sx={{ borderBottom: "1px solid #181e50" }}
+              sx={{ borderBottom: '1px solid #181e50' }}
               expandIcon={<ExpandMoreIcon />}
               aria-controls={`panel${index}a-content`}
               id={`panel${index}a-header`}
@@ -33,40 +33,38 @@ export default function SimpleAccordion(props) {
             </AccordionSummary>
 
             <AccordionDetails className="accordNoteWeek">
-              <Typography>
-                {itemArr.map((item, index) => {
-                  return (
-                    <Accordion key={index} sx={{ border: "1px solid #181e50" }}>
-                      <AccordionSummary
-                        sx={{ borderBottom: "1px solid #181e50" }}
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls={`panel${index}a-content`}
-                        id={`panel${index}a-header`}
-                      >
-                        <Typography>Day: {item.day}</Typography>
-                      </AccordionSummary>
+              {itemArr.map((item, index) => {
+                return (
+                  <Accordion key={index} sx={{ border: '1px solid #181e50' }}>
+                    <AccordionSummary
+                      sx={{ borderBottom: '1px solid #181e50' }}
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls={`panel${index}a-content`}
+                      id={`panel${index}a-header`}
+                    >
+                      <Typography>Day: {item.day}</Typography>
+                    </AccordionSummary>
 
-                      <AccordionDetails className="accordNote">
-                        <Typography>{item.note}</Typography>
-                        <ol className="tagList">
-                          {item.tags.map((item, index) => {
-                            return (
-                              <li key={index}>
-                                <img
-                                  alt="tag"
-                                  className="imageTag"
-                                  src={Tag}
-                                ></img>
-                                {item}
-                              </li>
-                            );
-                          })}
-                        </ol>
-                      </AccordionDetails>
-                    </Accordion>
-                  );
-                })}
-              </Typography>
+                    <AccordionDetails className="accordNote">
+                      <Typography>{item.note}</Typography>
+                      <ol className="tagList">
+                        {item.tags.map((item, index) => {
+                          return (
+                            <li key={index}>
+                              <img
+                                alt="tag"
+                                className="imageTag"
+                                src={Tag}
+                              ></img>
+                              {item}
+                            </li>
+                          );
+                        })}
+                      </ol>
+                    </AccordionDetails>
+                  </Accordion>
+                );
+              })}
             </AccordionDetails>
           </Accordion>
         );
